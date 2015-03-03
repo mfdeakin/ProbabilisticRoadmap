ad763a2b1934839fcff9f51b9ce4ae28e4cf4cb8
Michael Deakin <mfdeakin@cs.unc.edu>
2015-03-02 21:36:27 -0500

    Added cairo drawing, so I don't have to do it by hand

--- prob_roadmap.py
+++ prob_roadmap.py
@@ -1,7 +1,9 @@
 
-from math import copysign, sqrt
+from math import copysign, sqrt, pi
 from random import randint
 from time import sleep
+import numpy as np
+import cairo
 
 class Vector():
     def __init__(self, x, y):
@@ -81,21 +83,57 @@ def buildPRM(rects, src, dest, xMax, yMax):
     graphTC = {}
     graphTC[src] = {src: 0}
     graphTC[dest] = {dest: 0}
+    
+    imScale = 32
+    imXDim = imScale * xMax
+    imYDim = imScale * yMax
+    rmBmp = np.zeros((imXDim, imYDim, 4), dtype=np.uint8)
+    rmSurf = cairo.ImageSurface.create_for_data(rmBmp,
+                                                cairo.FORMAT_ARGB32,
+                                                imXDim, imYDim)
+    rmCtx = cairo.Context(rmSurf)
+    rmCtx.set_source_rgb(1.0, 1.0, 1.0)
+    rmCtx.translate(0, imYDim)
+    rmCtx.scale(imScale, -imScale)
+    rmCtx.paint()
+    rmCtx.set_line_width(0.2)
+    rmCtx.set_source_rgb(0.0, 1.0, 0.0)
+    rmCtx.arc(dest.x, dest.y, 0.5, 0, 2 * pi)
+    rmCtx.stroke()
+    rmCtx.set_source_rgb(0.0, 0.0, 1.0)
+    rmCtx.arc(src.x, src.y, 0.5, 0, 2 * pi)
+    rmCtx.stroke()
+    rmCtx.set_source_rgb(0.0, 0.0, 0.0)
+    for r in rects:
+        rmCtx.rectangle(r.xMin, r.yMin, r.xMax - r.xMin, r.yMax - r.yMin)
+        rmCtx.stroke()
+    
     if Link(rects, src, dest):
         dist = distance(src, dest)
         graphTC[src][dest] = dist
         graphTC[dest][src] = dist
     while dest not in graphTC[src].keys():
-        newPt = Vector(randint(0, xMax), randint(0, yMax))
+        newPt = Vector(randint(1, xMax - 1), randint(1, yMax - 1))
         if not Clear(rects, newPt) or newPt in graphTC.keys():
             continue
+        
+        rmCtx.set_source_rgb(1.0, 0.0, 0.0)
+        rmCtx.arc(newPt.x, newPt.y, 0.5, 0, 2 * pi)
+        rmCtx.stroke()
+        
         print("Adding Point", len(graphTC), newPt)
         graphTC[newPt] = {}
         # First find the points it can connect to
+        rmCtx.set_source_rgb(0.0, 0.0, 0.0)
         for pt in graphTC.keys():
             test = Link(rects, pt, newPt)
             print("Linking", newPt, pt, test)
             if test:
+                
+                rmCtx.move_to(newPt.x, newPt.y)
+                rmCtx.line_to(pt.x, pt.y)
+                rmCtx.stroke()
+                
                 dist = distance(pt, newPt)
                 graphTC[pt][newPt] = dist
                 graphTC[newPt][pt] = dist
@@ -123,19 +161,21 @@ def buildPRM(rects, src, dest, xMax, yMax):
                     graphTC[pt][pt2] = dist
     print(len(graphTC))
     print(graphTC)
-    return graphTC
+    return rmSurf
 
 def runTest():
     rects1 = [Rect(6, 13, 14, 22), Rect(4, 12, 0, 8),
               Rect(14, 22, 4, 10)]
     src1 = Vector(2, 2)
     dest1 = Vector(14, 21)
-    buildPRM(rects1, src1, dest1, 22, 22)
+    im1 = buildPRM(rects1, src1, dest1, 22, 22)
+    im1.write_to_png("Test_1.png")
     rects2 = [Rect(4, 16, 0, 12), Rect(6, 13, 14, 22),
               Rect(14, 22, 4, 10)]
     src2 = Vector(2, 2)
     dest2 = Vector(14, 21)
-    buildPRM(rects2, src2, dest2, 22, 22)
+    im2 = buildPRM(rects2, src2, dest2, 22, 22)
+    im2.write_to_png("Test_2.png")
 
 if __name__ == "__main__":
     runTest()
